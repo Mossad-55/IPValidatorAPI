9b72242d0c2e0d26e26d6eb60b8836ba7080d525
Mossad <mosad55522@gmail.com>
2026-04-08 13:49:56 +0200

    Created IP Controller

---
+++ Presentation/Controllers/IpController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/ip")]
+[ApiController]
+public class IpController : ControllerBase
+{
+    private readonly IpApplicationService _service;
+
+    public IpController(IpApplicationService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("lookup")]
+    public async Task<IActionResult> Lookup([FromQuery] string? ipAddress)
+    {
+        var ip = ipAddress ?? HttpContext.Connection.RemoteIpAddress?.ToString();
+
+        var result = await _service.LookupAsync(ip!);
+
+        return Ok(result);
+    }
+
+    [HttpGet("check-block")]
+    public async Task<IActionResult> CheckBlock()
+    {
+        var ip = HttpContext.Connection.RemoteIpAddress?.ToString();
+        var userAgent = Request.Headers["User-Agent"].ToString();
+
+        var isBlocked = await _service.CheckIfBlockedAsync(ip!, userAgent);
+
+        return Ok(new { isBlocked = isBlocked });
+    }
+}
