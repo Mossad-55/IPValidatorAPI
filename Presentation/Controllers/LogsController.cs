8a7548ae160a8da9d020df2614a9307379dc5670
Mossad <mosad55522@gmail.com>
2026-04-08 13:57:47 +0200

    Created Logs Controller

---
+++ Presentation/Controllers/LogsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Application.RequestFeatures;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/logs")]
+[ApiController]
+public class LogsController : ControllerBase
+{
+    private readonly IServiceManager _service;
+    public LogsController(IServiceManager service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("blocked-attempts")]
+    public async Task<IActionResult> GetLogs([FromQuery] PaginationRequest request)
+    {
+        var result = await _service.LogService.GetAllLogsAsync(request);
+
+        return Ok(result);
+    }
+}
